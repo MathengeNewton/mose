2b9933809a1233826a69b66ef685c32cf1e839af
Mathenge Newton <45616070+MathengeNewton@users.noreply.github.com>
2020-04-01 06:20:12 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-DEBUG = True
-SECRET_KEY = 'thismightbemysecretkey'
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:mathenge,./1998@localhost/mainapp'
-UPLOADED_IMAGE_DEST = "C:/projects/mose/frontend-api/static/uploads/images/"
-UPLOADED_IMAGE_URL = "C:/projects/mose/frontend-api/static/uploads/profiles/"
-ALLOWED_IMAGE_EXTENSIONS = ['PNG', 'JPG', 'JPEG', 'GIF']
